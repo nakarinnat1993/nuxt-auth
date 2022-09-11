3d0b05d2fabf161f3c488fa2081d912f2b518b5e
Nakarin Jaiseengam <nakarinnat1993@gmail.com>
2022-09-11 14:28:02 +0700

    Add page middleware auth

---
+++ pages/private.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>THiS IS PRIVATE</h1>
+
+        <nuxt-link to="/">Go to Home</nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+    middleware: 'auth',
+}
+</script>
